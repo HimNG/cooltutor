3ad92bb72549d3fb9a5b4ac88499f94c678fb86d
HimNG <warriornikky@gmail.com>
2016-12-08 22:50:24 +0530

    Added Changes index.php for Demo videos. In Home Screen , Demo video is getting Displayed.
    Signed-off-by: HimNG <warriornikky@gmail.com>

--- index.php
+++ index.php
@@ -3,64 +3,80 @@
 <title> NEW TASK </title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/index.css">
-
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.min.js"></script>
 </head>
 <body>
-
-<div class="navbar navbar-inverse navbar-fixed-left col-md-3">
-  <a class="navbar-brand" href="#">KulTutor </a>
-  <ul class="nav navbar-nav">
-   <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">6th Standard <span class="caret"></span></a>
-     <ul class="dropdown-menu" role="menu">
-      <li><a href="#">Mathematics</a></li>
-      <li><a href="#">Science</a></li>
-      <!--<li><a href="#">Sub Menu3</a></li>
-      <li class="divider"></li>
-      <li><a href="#">Sub Menu4</a></li>
-      <li><a href="#">Sub Menu5</a></li>-->
-     </ul>
-   </li>
-   <li><a href="#">7th Standard</a></li>
-   <li><a href="#">8th Standard</a></li>
-   <li><a href="#">9th Standard</a></li>
-   <li><a href="#">10th Standard</a></li>
-  </ul>
-</div>
-<div class="container col-md-12">
- <div class="row">
-   <div class="wrapper-fluid">
+<!--<div class="container">-->
+<nav class="navbar navbar-default">
   <div class="container-fluid">
-    <div class="row ">
-      <div id="screen1" class="col-md-3">
-        <!-- PAGE 1 HERE --> 
-		<img id="image" src="images/Koala.jpg" alt="Mountain View" >
-		<a html="https://www.youtube.com/watch?v=VGYC8LBaVIQ"></a>
-		
-      </div>
-      <div id="screen2" class="col-md-3">
-        <!-- PAGE 2 HERE -->  
-		<img id="image" src="images/Penguins.jpg" alt="Mountain View" >
-		<a html="https://www.youtube.com/watch?v=VGYC8LBaVIQ"></a>
-		
-      </div>
-	    <div id="screen1" class="col-md-3">
-        <!-- PAGE 3 HERE --> 
-		<img id="image" src="images/Chrysanthemum.jpg" alt="Mountain View" >
-		<a html="https://www.youtube.com/watch?v=VGYC8LBaVIQ"></a>
-		
-      </div>
-      <div id="screen2" class="col-md-3">
-        <!-- PAGE 4 HERE -->  
-		<img id="image" src="images/Desert.jpg" alt="Mountain View" >
-		<a html="https://www.youtube.com/watch?v=VGYC8LBaVIQ"></a>
-		
-      </div>
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">KulTutor &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
     </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">link1</a></li>
+      <li><a href="#">link2</a></li>
+      <li><a href="#">link3</a></li>
+    </ul>
   </div>
+</nav>
+
+<div class="navbar navbar-inverse navbar-fixed-left col-lg-4">
+	<!--<div class="container">-->
+	<ul class="nav navbar-nav">
+		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">6th Standard <span class="caret"></span></a>
+			<ul class="dropdown-menu" role="menu">
+				<li><a href="#">Mathematics</a></li>
+				<li><a href="#">Science</a></li>
+				<!--<li><a href="#">Sub Menu3</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Sub Menu4</a></li>
+				<li><a href="#">Sub Menu5</a></li>-->
+			</ul>
+		</li>
+		<li><a href="#">7th Standard</a></li>
+		<li><a href="#">8th Standard</a></li>
+		<li><a href="#">9th Standard</a></li>
+		<li><a href="#">10th Standard</a></li>
+	</ul>
+	<!--</div>-->
 </div>
-   
-   
+<div class="container col-lg-8">
+	<div class="row">
+		<!--<div class="wrapper-fluid">-->
+			<!--<div class="container-fluid">-->
+				<div class="row">
+					<div class="col-md-4">
+						<!-- PAGE 1 HERE --> 
+						<iframe src="https://www.youtube.com/embed/pX2_JxVRzBM" allowfullscreen></iframe>
+						<p>Time Rate and Distance Questions</p>
+					</div>
+					<div class=col-md-4></div>
+				    <div class="col-md-4">
+						<!-- PAGE 2 HERE -->  
+						<iframe src="https://www.youtube.com/embed/pX2_JxVRzBM" allowfullscreen></iframe>
+						<p>Time Rate and Distance Questions</p>
+					</div>
+				</div>
+				<br /><br /><br />
+				<div class="row ">
+					<div class="col-md-4">
+						<!-- PAGE 3 HERE --> 
+						<iframe src="https://www.youtube.com/embed/pX2_JxVRzBM" allowfullscreen></iframe>
+						<p>Time Rate and Distance Questions</p>
+					</div>
+					<div class=col-md-4></div>
+					<div class="col-md-4">
+						<!-- PAGE 4 HERE -->  
+						<iframe src="https://www.youtube.com/embed/pX2_JxVRzBM" allowfullscreen></iframe>
+						<p>Time Rate and Distance Questions</p>
+					</div>
+				</div>
+			<!--</div>-->
+		<!--</div>-->
+	</div>
  </div>
- </div>
+ <!--</div>-->
  </body>
 </html>
